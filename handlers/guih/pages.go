e6ea2111c34e9d7b1f12531531b3fdab9365da34
M. E. Abdelsalam <mo.ehab150@gmail.com>
2024-11-04 05:13:54 +0200

    fix: error fixed in IndexPage handler.

--- handlers/guih/pages.go
+++ handlers/guih/pages.go
@@ -11,13 +11,22 @@ import (
 func IndexPage(isadmin bool) func(*fiber.Ctx) error {
   return func(c *fiber.Ctx) error {
     c.Set(fiber.HeaderContentType, fiber.MIMETextHTML)
-    data, err := streamers.Get(c.Params("streamer_id"))
+
+    var data streamers.DataModel
+    var err error
+    if isadmin {
+      data, err = streamers.Get(c.Cookies("streamer_id"))
+    } else {
+      data, err = streamers.Get(c.Params("streamer_id"))
+    }
+
     if err != nil {
       return c.SendStatus(fiber.StatusInternalServerError)
     }
     if (data.Id == "") {
       return c.SendStatus(fiber.StatusNotFound)
     }
+
     pages.Index(&data, isadmin).Render(context.Background(), c.Response().BodyWriter())
     return c.SendStatus(200)
   }
