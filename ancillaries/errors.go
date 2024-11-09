505acb3ebe3582d0c2a993e7dcc1db4adc132bba
M. E. Abdelsalam <mo.ehab150@gmail.com>
2024-11-09 06:53:42 +0200

    Must and Recover functions added to ancillaries package in order to make the code of the rest of the application more readable.

---
+++ ancillaries/errors.go
@@ -0,0 +1,29 @@
+package ancillaries
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// ancillary function used to clean the code in handlers;
+// by wrapping db methods and return the result if no error
+// is found, or panic otherwise.
+func Must(res any, err error) any {
+  if err != nil {
+    panic(err)
+  }
+  return res
+}
+
+// ancillary function that shall be used (deferred) in the 
+// beginning of functions where anc.Must is used within.
+func Recover(c *fiber.Ctx) error {
+  if r := recover(); r != nil {
+    log.Println(r)
+    Notify(c, "Something went wrong!", "bg-error")
+    return c.SendStatus(fiber.StatusInternalServerError)
+  }
+  return nil
+}
+
--- ancillaries/handlers.go
+++ ancillaries/handlers.go
@@ -7,7 +7,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// an ancillary function whose only purpose it to follow the DRY principle!
+// an ancillary function whose only purpose is to follow the DRY principle!
 // it shall be used in handler function wherever a notification should be
 // rendered (htmx) to the user.
 func Notify(c *fiber.Ctx, msg, color string) {
