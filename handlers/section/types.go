6c3dcc2996577e11cb64b50baa6ac99430577903
M. E. Abdelsalam <mo.ehab150@gmail.com>
2024-11-11 17:10:26 +0200

    impl: vidoe and channel forms functionality implemented.

--- handlers/section/types.go
+++ handlers/section/types.go
@@ -2,5 +2,5 @@ package section
 
 type SectionBody struct {
   ChannelId int `json:"channel_id" xml:"channel_id" form:"channel_id"`
-  Name string `json:"name" xml:"name" form:"name"`
+  Name string `json:"Section Name" xml:"Section Name" form:"Section Name"`
 }
--- handlers/video/types.go
+++ handlers/video/types.go
@@ -2,6 +2,6 @@ package video
 
 type VideoBody struct {
   SectionId int `json:"section_id" xml:"section_id" form:"section_id"`
-  Title string `json:"title" xml:"title" form:"title"`
-  Url string `json:"url" xml:"url" form:"url"`
+  Title string `json:"Video Title" xml:"Video Title" form:"Video Title"`
+  Url string `json:"Embedded Url" xml:"Embedded Url" form:"Embedded Url"`
 }
